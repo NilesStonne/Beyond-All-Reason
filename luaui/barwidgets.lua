cb1120ed88d4ec2af874ff1a44cf531aafd935e9
Daniel Harvey <keithdanielharvey@gmail.com>
2026-07-04 00:32:17 -0600

    Sharing 6/7: Sharing Tab UI

--- luaui/barwidgets.lua
+++ luaui/barwidgets.lua
@@ -213,6 +213,7 @@ local callInLists = {
 	'CommandsChanged',
 	'LanguageChanged',
 	'UnitBlocked',
+	'SharePolicyChanged',
 	'VisibleUnitAdded',
 	'VisibleUnitRemoved',
 	'VisibleUnitsChanged',
@@ -2402,6 +2403,14 @@ function widgetHandler:UnitBlocked(unitDefID, reasons)
 	tracy.ZoneBeginN("W:UnitBlocked")
 	for _, w in ipairs(self.UnitBlockedList) do
 		w:UnitBlocked(unitDefID, reasons)
+	end
+	tracy.ZoneEnd()
+end
+
+function widgetHandler:SharePolicyChanged(teamID, domain)
+	tracy.ZoneBeginN("W:SharePolicyChanged")
+	for _, w in ipairs(self.SharePolicyChangedList) do
+		w:SharePolicyChanged(teamID, domain)
 	end
 	tracy.ZoneEnd()
 end
